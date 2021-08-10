6ef1bc9b506557c3152f10e2e85c8f9c5035c5c5
Illyoung Choi <iychoi@email.arizona.edu>
2021-08-10 16:34:35 -0700

    Implement ticket

---
+++ irods/message/ticketadmin_response.go
@@ -0,0 +1,31 @@
+package message
+
+import (
+	"fmt"
+
+	"github.com/cyverse/go-irodsclient/irods/common"
+)
+
+// IRODSMessageTicketAdminResponse stores ticket admin response
+type IRODSMessageTicketAdminResponse struct {
+	// empty structure
+	Result int
+}
+
+// CheckError returns error if server returned an error
+func (msg *IRODSMessageTicketAdminResponse) CheckError() error {
+	if msg.Result < 0 {
+		return common.MakeIRODSError(common.ErrorCode(msg.Result))
+	}
+	return nil
+}
+
+// FromMessage returns struct from IRODSMessage
+func (msg *IRODSMessageTicketAdminResponse) FromMessage(msgIn *IRODSMessage) error {
+	if msgIn.Body == nil {
+		return fmt.Errorf("Cannot create a struct from an empty body")
+	}
+
+	msg.Result = int(msgIn.Body.IntInfo)
+	return nil
+}
