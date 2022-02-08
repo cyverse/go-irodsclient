e124797927f000d073f77b8136ad257b5e29f60b
Illyoung Choi <iychoi@email.arizona.edu>
2022-02-08 15:22:43 -0700

    Add GetTicketForAnonymousAccess API to FS layer

--- fs/fs.go
+++ fs/fs.go
@@ -1612,3 +1612,19 @@ func (fs *FileSystem) ListUserMetadata(user string) ([]*types.IRODSMeta, error) 
 
 	return metadataobjects, nil
 }
+
+// GetTicketForAnonymousAccess gets ticket information for anonymous access
+func (fs *FileSystem) GetTicketForAnonymousAccess(ticket string) (*types.IRODSTicketForAnonymousAccess, error) {
+	conn, err := fs.session.AcquireConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer fs.session.ReturnConnection(conn)
+
+	ticketInfo, err := irods_fs.GetTicketForAnonymousAccess(conn, ticket)
+	if err != nil {
+		return nil, err
+	}
+
+	return ticketInfo, err
+}
