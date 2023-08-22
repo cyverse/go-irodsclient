c28304fc2c275e6db01d5dae490fa156eb13500b
Illyoung Choi <iychoi@email.arizona.edu>
2023-08-22 09:20:51 -0700

    add r/w lock

--- fs/fs_lock.go
+++ fs/fs_lock.go
@@ -1,16 +1,14 @@
 package fs
 
 import (
-	"strings"
-
 	irods_fs "github.com/cyverse/go-irodsclient/irods/fs"
 	"github.com/cyverse/go-irodsclient/irods/types"
 	"github.com/cyverse/go-irodsclient/irods/util"
 	"github.com/rs/xid"
 )
 
-// LockDataObject locks data object
-func (fs *FileSystem) LockDataObject(path string, lockMode string, wait bool) (*FileLockHandle, error) {
+// WLockDataObject locks data object with write lock (exclusive)
+func (fs *FileSystem) WLockDataObject(path string, wait bool) (*FileLockHandle, error) {
 	irodsPath := util.GetCorrectIRODSPath(path)
 
 	stat, err := fs.Stat(irodsPath)
@@ -24,13 +22,47 @@ func (fs *FileSystem) LockDataObject(path string, lockMode string, wait bool) (*
 	}
 
 	lockType := types.DataObjectLockTypeWrite
-	switch strings.ToLower(lockMode) {
-	case "r", "ro", "readonly":
-		lockType = types.DataObjectLockTypeRead
-	case "w", "rw", "wo", "readwrite", "writeonly":
-		lockType = types.DataObjectLockTypeWrite
+	lockCommand := types.DataObjectLockCommandSetLock
+	if wait {
+		lockCommand = types.DataObjectLockCommandSetLockWait
 	}
 
+	handle, err := irods_fs.LockDataObject(conn, irodsPath, lockType, lockCommand)
+	if err != nil {
+		fs.ioSession.ReturnConnection(conn)
+		return nil, err
+	}
+
+	// do not return connection here
+	fileLockHandle := &FileLockHandle{
+		id:                  xid.New().String(),
+		filesystem:          fs,
+		connection:          conn,
+		irodsfilelockhandle: handle,
+		entry:               stat,
+		lockType:            lockType,
+		lockCommand:         lockCommand,
+	}
+
+	fs.fileLockHandleMap.Add(fileLockHandle)
+	return fileLockHandle, nil
+}
+
+// RLockDataObject locks data object with read lock
+func (fs *FileSystem) RLockDataObject(path string, wait bool) (*FileLockHandle, error) {
+	irodsPath := util.GetCorrectIRODSPath(path)
+
+	stat, err := fs.Stat(irodsPath)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := fs.ioSession.AcquireConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	lockType := types.DataObjectLockTypeRead
 	lockCommand := types.DataObjectLockCommandSetLock
 	if wait {
 		lockCommand = types.DataObjectLockCommandSetLockWait
