ea9e61aa6343217f064d56c0a9cf3bd292564ea6
Illyoung Choi <iychoi@email.arizona.edu>
2023-09-07 15:49:52 -0700

    Add more password obfuscation test

--- test/testcases/password_obfuscation_test.go
+++ test/testcases/password_obfuscation_test.go
@@ -5,11 +5,13 @@ import (
 	"testing"
 
 	"github.com/cyverse/go-irodsclient/utils/icommands"
+	"github.com/sethvargo/go-password/password"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPasswordObfuscation(t *testing.T) {
 	t.Run("test PasswordObfuscation", testEncodeDecodePassword)
+	t.Run("test EncodeDecodeRandomPassword", testEncodeDecodeRandomPassword)
 }
 
 func testEncodeDecodePassword(t *testing.T) {
@@ -36,4 +38,22 @@ func testEncodeDecodePassword(t *testing.T) {
 
 	//t.Logf("enc: %s", encodedPassword)
 	//t.Logf("dec: %s", decodedPassword)
+}
+
+func testEncodeDecodeRandomPassword(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		mypassword, err := password.Generate(20, 10, 10, false, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		//t.Logf("test password '%s'", mypassword)
+
+		encodedPassword := icommands.EncodePasswordString(mypassword, 1000)
+		decodedPassword := icommands.DecodePasswordString(encodedPassword, 1000)
+
+		//t.Logf("enc: %s", encodedPassword)
+		//t.Logf("dec: %s", decodedPassword)
+
+		assert.Equal(t, mypassword, decodedPassword)
+	}
 }
