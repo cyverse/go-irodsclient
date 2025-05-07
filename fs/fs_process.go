472d5351cc1ee967f74e8282cf92a63439deb4a1
Illyoung Choi <iychoi@email.arizona.edu>
2025-05-07 14:13:08 -0700

    add StatProcess API

---
+++ fs/fs_process.go
@@ -0,0 +1,22 @@
+package fs
+
+import (
+	irods_fs "github.com/cyverse/go-irodsclient/irods/fs"
+	"github.com/cyverse/go-irodsclient/irods/types"
+)
+
+// StatProcess stats processes
+func (fs *FileSystem) StatProcess(address string, zoneName string) ([]*types.IRODSProcess, error) {
+	conn, err := fs.metadataSession.AcquireConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer fs.metadataSession.ReturnConnection(conn) //nolint
+
+	processes, err := irods_fs.StatProcess(conn, address, zoneName)
+	if err != nil {
+		return nil, err
+	}
+
+	return processes, err
+}
